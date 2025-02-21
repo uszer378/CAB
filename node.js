e3b4e40dd551f02ef269fa458adc1490226ab459
uszer378 <mY_s0n_Sso0_dum-iq21@hotmail.com>
2025-02-20 20:24:25 -0500

    Update node.js

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 <script>
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     // User is on a mobile device
-    window.location.href = "https://m.yourwebsite.com"; // Replace with your mobile URL
+    window.location.href = "https://uszer378.github.io/CAB/m/index.html"; // Replace with your mobile URL
   } else {
     // User is on a desktop or other device
     // (Optional: You can add code here for desktop users)
