58dc2631b7f6b1503baa258b096e3f5529b47090
uszer378 <mY_s0n_Sso0_dum-iq21@hotmail.com>
2025-02-20 20:02:45 -0500

    Add files via upload

---
+++ node.js
@@ -0,0 +1,9 @@
+<script>
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // User is on a mobile device
+    window.location.href = "https://m.yourwebsite.com"; // Replace with your mobile URL
+  } else {
+    // User is on a desktop or other device
+    // (Optional: You can add code here for desktop users)
+  }
+</script>
