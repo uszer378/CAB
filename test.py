59faff21886aece38b0146ad5ffda7486374d762
uszer378 <mY_s0n_Sso0_dum-iq21@hotmail.com>
2025-05-11 16:45:42 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+    <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+  </head>
+  <body>
+    <py-script>
+      print("just testing python")
+    </py-script>
+  </body>
+</html>
+
